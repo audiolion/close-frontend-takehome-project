e32c77dfb70b4a3f9bd3572922616d5630724fda
Ryan Castner <castner.rr@gmail.com>
2019-11-26 11:12:12 -0500

    Add KanbanCard component
    Article html tag was used as it represents repeated content.

---
+++ src/components/screens/Board/KanbanCard/KanbanCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import gravatarUrl from 'gravatar-url';
+import styles from './kanban-card.module.css';
+
+export const KanbanCard = ({ title, content, authorEmail }) => {
+  return (
+    <article className={styles.card}>
+      <header className={styles.header}>
+        <img
+          width="24"
+          height="24"
+          alt={`${authorEmail} Avatar`}
+          src={gravatarUrl(authorEmail, { size: 24 })}
+        />
+        <h3 className={styles.heading}>{title}</h3>
+      </header>
+      <p className={styles.content}>{content}</p>
+    </article>
+  );
+};
