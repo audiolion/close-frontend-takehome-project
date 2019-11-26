a0d54d019606947d483c0308b5297ab934ab0e59
Ryan Castner <castner.rr@gmail.com>
2019-11-26 10:23:35 -0500

    Add KanbanColumn header
    To enhance the semantic HTML, the column is now a section element and the header is a header element.

--- src/components/screens/Board/KanbanColumn/KanbanColumn.js
+++ src/components/screens/Board/KanbanColumn/KanbanColumn.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import styles from './kanban-column.module.css';
 
-export const KanbanColumn = props => {
-  return <div className={styles.column}></div>;
+export const KanbanColumn = ({ title }) => {
+  return (
+    <section className={styles.column}>
+      <header className={styles.header}>
+        <h2 className={styles.heading}>{title}</h2>
+        <button type="button">
+          <svg
+            width="24"
+            height="24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M12 0v24M0 12h24" stroke="#222" stroke-width="4" />
+          </svg>
+        </button>
+      </header>
+    </section>
+  );
 };
