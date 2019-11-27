38d8e2220d95488169a519c1c5f909e5b7d7aabf
Ryan Castner <castner.rr@gmail.com>
2019-11-26 19:29:22 -0500

    Refactor React.memo components to have names
    Makes it easier to debug in devtools.

--- src/components/screens/Board/Avatar/Avatar.js
+++ src/components/screens/Board/Avatar/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import gravatarUrl from 'gravatar-url';
 
-export const Avatar = React.memo(({ email }) => {
+export const Avatar = React.memo(function Avatar({ email }) {
   return (
     <img
       width="24"
--- src/components/screens/Board/KanbanColumn/KanbanCard/KanbanCard.js
+++ src/components/screens/Board/KanbanColumn/KanbanCard/KanbanCard.js
@@ -3,29 +3,33 @@ import { Draggable } from 'react-beautiful-dnd';
 import { Avatar } from '../../Avatar';
 import styles from './kanban-card.module.css';
 
-export const KanbanCard = React.memo(
-  ({ cardId, index, title, description, authorEmail }) => {
-    return (
-      <Draggable draggableId={cardId} index={index}>
-        {provided => (
-          <article
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            className={styles.card}
-            ref={provided.innerRef}
-          >
-            <header className={styles.header}>
-              <Avatar email={authorEmail} />
-              <h3 className={styles.heading}>{title}</h3>
-            </header>
-            <p className={styles.description}>{description}</p>
-          </article>
-        )}
-      </Draggable>
-    );
-  },
-  arePropsEqual,
-);
+export const KanbanCard = React.memo(function KanbanCard({
+  cardId,
+  index,
+  title,
+  description,
+  authorEmail,
+}) {
+  return (
+    <Draggable draggableId={cardId} index={index}>
+      {provided => (
+        <article
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className={styles.card}
+          ref={provided.innerRef}
+        >
+          <header className={styles.header}>
+            <Avatar email={authorEmail} />
+            <h3 className={styles.heading}>{title}</h3>
+          </header>
+          <p className={styles.description}>{description}</p>
+        </article>
+      )}
+    </Draggable>
+  );
+},
+arePropsEqual);
 
 function arePropsEqual(prevProps, nextProps) {
   return (
